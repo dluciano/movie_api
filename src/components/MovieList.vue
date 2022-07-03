3221d720a980f46e822d05ca16f0ff493ff51989
Dawlin Pena Luciano <dawmusic19@gmail.com>
2022-07-03 05:13:27 +0100

    implement list component

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,8 +1,36 @@
-<template>  
-  <div v-for="movie in movies.data" :key="movie.imdbID">
-    <div>{{movie.Title}}</div>
-    <div>{{movie.Year}}</div>
-    <div>{{movie.imdbID}}</div>
+<template>
+  <div class="my-3">
+    <div class="grid grid-cols-4 gap-4">
+      <a
+        v-for="movie in movies.data"
+        :key="movie.imdbID"
+        class="
+          inline-block
+          p-6
+          max-w-sm
+          bg-white
+          rounded-lg
+          border border-gray-200
+          shadow-md
+          hover:bg-gray-100
+          dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700
+        "
+      >
+        <div>{{ movie.Title }}</div>
+        <div>{{ movie.Year }}</div>
+        <div>{{ movie.imdbID }}</div>
+      </a>
+    </div>
+
+    <div class="flex justify-center items-center w-full">
+      <a v-on:click="goToPage(1)" class="ml-1">&lt;&lt;</a>
+      <a v-on:click="goToPage(currentPage - 1)">&lt;</a>
+      <div class="" v-for="index in pagesIndexes" :key="index">
+        <a v-on:click="goToPage(index)" class="ml-1">{{ index }}</a>
+      </div>
+      <a v-on:click="goToPage(currentPage + 1)" class="ml-1">&gt;</a>
+      <a v-on:click="goToPage(movies.total_pages)" class="ml-1">&gt;&gt;</a>
+    </div>
   </div>
 </template>
 
@@ -13,16 +41,41 @@ import { listMovies } from "../api/MoviesApi";
 export default defineComponent({
   name: "MovieList",
   async mounted() {
-    this.movies = await listMovies(this.currentPage);
+    this.goToPage(1);
   },
   data() {
     return {
       currentPage: 1,
+      pagesIndexes: Array<number>(),
       movies: {},
     } as {
       currentPage: number;
+      pagesIndexes: number[];
       movies: MovieListPage;
     };
+  },
+  computed: {
+    isFirstPage() {
+      this.currentPage === 1;
+    },
+  },
+  methods: {
+    async goToPage(pageIndex: number) {
+      if (pageIndex === 0 || pageIndex === this.movies.total_pages + 1) return;
+      this.currentPage = pageIndex;
+      this.movies = await listMovies(pageIndex);
+      this.pagesIndexes = [];
+      for (
+        var i =
+          this.currentPage + 10 <= this.movies.total_pages
+            ? this.currentPage
+            : this.movies.total_pages - 10;
+        i < this.movies.total_pages && i < this.currentPage + 10;
+        i++
+      ) {
+        this.pagesIndexes.push(i);
+      }
+    },
   },
 });
 </script>
