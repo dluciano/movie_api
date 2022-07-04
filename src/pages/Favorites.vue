a30ebaac719781f2456d53f75496dd4d9e1e3502
Dawlin Pena Luciano <dawmusic19@gmail.com>
2022-07-04 01:36:32 +0100

    list simple details of fav movies (retrieved from store)

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -1,3 +1,22 @@
 <template>
+  <div>
     <h1>Favorites</h1>
+    <div v-for="movie in store.favMovies" :key="movie.imdbID">
+      <p>{{ movie.Title }}</p>
+    </div>
+  </div>
 </template>
+<script lang="ts">
+import { useMovieStore } from "@/store";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  async setup() {
+    const store  = useMovieStore();
+    await store.loadMoviesAsync();
+    return {
+      store,
+    };
+  },
+});
+</script>
