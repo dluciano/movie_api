4b7ce6fb14f7a92115f7e77531b0e58aa5e99460
Dawlin Pena Luciano <dawmusic19@gmail.com>
2022-07-04 04:36:23 +0100

    add main template design

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,45 @@
 <template>
   <div class="container mx-auto">
-    <div id="nav">
+    <header id="nav">
       <router-link to="/" v-slot="{ isActive, isExactActive }">
         <p
-        class="inline-block mr-3"
-          :class="[
-            isActive && 'underline',
-            isExactActive && 'underline',
-          ]"
+          class="inline-block mr-3"
+          :class="[isActive && 'underline', isExactActive && 'underline']"
         >
           Home
         </p>
       </router-link>
-      
+
       <router-link to="/fav" v-slot="{ isActive, isExactActive }">
         <p
-        class="inline-block"
-          :class="[
-            isActive && 'underline',
-            isExactActive && 'underline',
-          ]"
+          class="inline-block"
+          :class="[isActive && 'underline', isExactActive && 'underline']"
         >
           Favorites
-        </p></router-link>
-    </div>
+        </p></router-link
+      >
+    </header>
     <router-view v-slot="{ Component, route }">
       <suspense>
         <template #default>
-          <div>
+          <main class="container">
+            <h1 class="font-bold text-2xl mb-0">IMDB Movies Searcher</h1>
+            <p class="font-light">
+              List of movies registered in IMDB. Search your loved movies and
+              then add it as favorite
+            </p>
             <component
               :is="Component"
               :key="route.meta.usePathKey ? route.path : undefined"
               view-prop="value"
             />
-          </div>
+          </main>
         </template>
         <template #fallback><div>Loading...</div> </template>
       </suspense>
     </router-view>
   </div>
+  <footer class="w-full text-center bottom-0 absolute mb-3">Dawlin&copy; 2022</footer>
 </template>
 
 <script lang="ts">
